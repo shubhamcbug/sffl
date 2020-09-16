43e0d5e1b3a40b760be23e58afb7b46291d451cd
tksundar <53440744+tksundar@users.noreply.github.com>
2020-09-16 18:31:39 +0530

    added credits

--- client/src/components/GenericTable.js
+++ client/src/components/GenericTable.js
@@ -1,6 +1,7 @@
 /**
  * Created by Sundar on 07-09-2020.
  * email tksrajan@gmail.com
+ * Courtsey : https://gist.github.com/Lbatson/b42a30e49e6ddb946f55d26e8c895d75#file-view-js
  */
 import _ from 'lodash';
 import React, { Component } from 'react';
