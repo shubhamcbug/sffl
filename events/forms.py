d294a1bf70bff2d8f70848f7ab43a899cd60db28
tksundar <53440744+tksundar@users.noreply.github.com>
2020-09-27 10:34:23 +0530

    Form validation added

--- events/forms.py
+++ events/forms.py
@@ -6,3 +6,29 @@ from django import forms
 
 class FileUploadForm(forms.Form):
     files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=20)
+    password = forms.CharField(max_length=20)
+
+
+class NewUserForm(forms.Form):
+    username = forms.CharField(max_length=20)
+    password = forms.CharField(max_length=20)
+    email = forms.CharField(max_length=20)
+    mobile = forms.CharField(max_length=15)
+
+class EventRegistrationForm(forms.Form):
+    event_name = forms.CharField(max_length=50)
+    name = forms.CharField(max_length=40)
+    payment_amount = forms.CharField(max_length=5)
+    payment_ref = forms.CharField(max_length=10)
+    num_guests = forms.IntegerField()
+    num_days = forms.IntegerField()
+    mode_of_travel = forms.CharField(max_length=5)
+    arrival_date = forms.DateField()
+    arrival_time = forms.TimeField()
+    departure_date = forms.DateField()
+    pickup = forms.CharField(max_length=5)
+
--- events/views.py
+++ events/views.py
@@ -9,7 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
 from rest_framework.renderers import JSONRenderer
 
-from .forms import FileUploadForm
+from .forms import FileUploadForm, LoginForm, NewUserForm, EventRegistrationForm
 from .models import Event, Login, Registration, File_uploads
 from .serializers import EventSerializer, RegistrationSerializer, LoginSerializer
 
@@ -54,13 +54,17 @@ def user_is_valid(user, password):
 
 @csrf_exempt
 def login(request):
-    LOGGER.debug('request received for login')
-    if request.POST:
-        user = request.POST['username']
-        password = request.POST['password']
-        LOGGER.debug('login attempt with user %s and password %s' % (user, make_password(password)))
-        response = user_is_valid(user, password)
-        return HttpResponse(response)
+    LOGGER.debug('request received for login with method as %s' % request.method)
+    if request.method == 'POST':
+        loginForm = LoginForm(request.POST)
+        if loginForm.is_valid():
+            user = loginForm.cleaned_data['username']
+            password = loginForm.cleaned_data['password']
+            LOGGER.debug('login attempt with user %s and password %s' % (user, make_password(password)))
+            response = user_is_valid(user, password)
+            return HttpResponse(response)
+        else:
+            print(loginForm.errors)
     else:
         raise Http404('Invalid method GET')
 
@@ -80,35 +84,39 @@ def valid_registration(user, password, email, mobile):
 
 @csrf_exempt
 def register(request):
-    if request.POST:
-        user = request.POST['username']
-        password = request.POST['password']
-        email = request.POST['email']
-        mobile = request.POST['mobile']
-        LOGGER.debug('post values %s %s %s' % (user, email, mobile))
-        # save
-        status = valid_registration(user, password, email, mobile)
-        LOGGER.debug('Status is %s' % status)
-        if status == NEW:
-            LOGGER.debug('Creating new user ')
-            login_ = Login()
-            login_.name = user
-            login_.password = make_password(password)
-            login_.email = email
-            login_.mobile = mobile
-            login_.temp_password = ''
-            login_.save()
-            user = Login.objects.get(name__exact=user)
-            serializer = LoginSerializer(user)
-            content = JSONRenderer().render(serializer.data)
-            LOGGER.debug('response to new user registration is %s' % content)
-            return HttpResponse(content)
-        elif status == EXISTING:
-            LOGGER.debug('status existing ')
-            return HttpResponse(EXISTING)
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            mobile = form.cleaned_data['mobile']
+            LOGGER.debug('post values %s %s %s' % (user, email, mobile))
+            # save
+            status = valid_registration(user, password, email, mobile)
+            LOGGER.debug('Status is %s' % status)
+            if status == NEW:
+                LOGGER.debug('Creating new user ')
+                login_ = Login()
+                login_.name = user
+                login_.password = make_password(password)
+                login_.email = email
+                login_.mobile = mobile
+                login_.temp_password = ''
+                login_.save()
+                user = Login.objects.get(name__exact=user)
+                serializer = LoginSerializer(user)
+                content = JSONRenderer().render(serializer.data)
+                LOGGER.debug('response to new user registration is %s' % content)
+                return HttpResponse(content)
+            elif status == EXISTING:
+                LOGGER.debug('status existing ')
+                return HttpResponse(EXISTING)
+            else:
+                LOGGER.debug('status incomplete')
+                return HttpResponse(INCOMPLETE)
         else:
-            LOGGER.debug('status incomplete')
-            return HttpResponse(INCOMPLETE)
+            print(form.errors)
 
 
 @csrf_exempt
@@ -128,40 +136,42 @@ def registrations(request):
 
 @csrf_exempt
 def create(request):
-    if request.POST:
-        # retrieve parameters
-        event_name = request.POST['event_name']
-        event = Event.objects.get(event_name__exact=event_name)
-        name = request.POST['name']
-        payment_amount = request.POST['payment_amount']
-        payment_ref = request.POST['payment_ref']
-        num_guests = request.POST['num_guests']
-        num_days = request.POST['num_days']
-        mode_of_travel = request.POST['mode_of_travel']
-        arrival_date = request.POST['arrival_date']
-        arrival_time = request.POST['arrival_time']
-        departure_date = request.POST['departure_date']
-        pickup = request.POST['pickup']
-        # create the object
-        registration = Registration()
-        registration.event = event
-        registration.name = name
-        registration.payment_amount = payment_amount
-        registration.payment_ref = payment_ref
-        registration.num_guests = num_guests
-        registration.num_days = num_days
-        registration.mode_of_travel = mode_of_travel
-        registration.arrival_date = arrival_date
-        registration.arrival_time = arrival_time
-        registration.departure_date = departure_date
-        registration.pickup = pickup
-        # save to database
-        try:
-            registration.save()
-            LOGGER.debug('Registration successful')
-            return HttpResponse('{"update":"success"}')
-        except RuntimeError:
-            raise EnvironmentError('Database persist failed')
+    if request.method == 'POST':
+        form = EventRegistrationForm(request.POST)
+        if form.is_valid():
+            # retrieve parameters
+            event_name = form.cleaned_data['event_name']
+            event = Event.objects.get(event_name__exact=event_name)
+            name = form.cleaned_data['name']
+            payment_amount = form.cleaned_data['payment_amount']
+            payment_ref = form.cleaned_data['payment_ref']
+            num_guests = form.cleaned_data['num_guests']
+            num_days = form.cleaned_data['num_days']
+            mode_of_travel = form.cleaned_data['mode_of_travel']
+            arrival_date = form.cleaned_data['arrival_date']
+            arrival_time = form.cleaned_data['arrival_time']
+            departure_date = form.cleaned_data['departure_date']
+            pickup = form.cleaned_data['pickup']
+            # create the object
+            registration = Registration()
+            registration.event = event
+            registration.name = name
+            registration.payment_amount = payment_amount
+            registration.payment_ref = payment_ref
+            registration.num_guests = num_guests
+            registration.num_days = num_days
+            registration.mode_of_travel = mode_of_travel
+            registration.arrival_date = arrival_date
+            registration.arrival_time = arrival_time
+            registration.departure_date = departure_date
+            registration.pickup = pickup
+            # save to database
+            try:
+                registration.save()
+                LOGGER.debug('Registration successful')
+                return HttpResponse('{"update":"success"}')
+            except RuntimeError:
+                raise EnvironmentError('Database persist failed')
     else:
         raise Http404('Invalid method GET')
 
@@ -206,6 +216,7 @@ def upload_media(request):
         LOGGER.debug("%s" % content)
         return HttpResponse(content)
 
+
 @csrf_exempt
 def display_media(request):
     if request.method == 'POST':
@@ -215,10 +226,8 @@ def display_media(request):
         url_prefix = settings.MEDIA_URL
         urls = []
         for m in media:
-            urls.append(url_prefix+str(m.file_url))
+            urls.append(url_prefix + str(m.file_url))
         print(urls)
         content = json.dumps(urls)
         LOGGER.debug(content)
         return HttpResponse(content)
-
-
